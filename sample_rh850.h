57b484e7e104bbaeba23e330a7bb1d4c9826b478
Shubham Mandavkar <96900610+Shubham382@users.noreply.github.com>
2026-03-12 11:42:33 +0530

    SampleCode

---
+++ sample_rh850.h
@@ -0,0 +1,10 @@
+/* File: sample_rh850.h */
+#ifndef SAMPLE_RH850_H
+#define SAMPLE_RH850_H
+
+int add(int a, int b);
+int is_even(int x);
+int max(int a, int b);
+int check_conditions(int a, int b, int c);
+
+#endif /* SAMPLE_RH850_H */
